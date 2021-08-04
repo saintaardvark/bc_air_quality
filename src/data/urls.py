42f60f50d821270055b583e5b7b9c2eb819f3c78
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-08-04 07:07:44 -0700

    Add support for PM25 CSV file
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- src/data/urls.py
+++ src/data/urls.py
@@ -3,4 +3,5 @@
 
 ENVBC_HOST = "ftp.env.gov.bc.ca"
 RAW_AQ_DIR = "/pub/outgoing/AIR/Hourly_Raw_Air_Data/Year_to_Date/"
-CSV_FILES = {"PM10": "PM10.csv"}
+CSV_FILES = {"PM10": "PM10.csv",
+             "PM25": "PM25.csv"}
